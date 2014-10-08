eb4999f862176992fdd5be213f678881476bd590
Mohammad Kalboneh <renorens@yahoo.com>
2014-10-08 16:59:44 +0200

    added emergency contact edit

--- app/views/Patients/editPatient.blade.php
+++ app/views/Patients/editPatient.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.clinicMaster')
 @section('content')
-<div class="well well-lg">
+<div class="well">
   <div class="container">
     <div class="row">
         <div class="col-md-6">
@@ -13,7 +13,7 @@
         </div>
         <div class="col-md-6">
           <h2>Contact Info</h2>
-          <div class="container">
+          <!-- <div class="container"> -->
             <div class="row">
               <div class="col-sm-3">
               @if ($patient->address != '')
@@ -38,8 +38,8 @@
               @endif
               </div>
               <div class="col-sm-3">
-                <button class="btn btn-success btn-sm" data-toggle="modal" data-target="#delete_confirmation"><i class="fa fa-edit fa-lg"></i>&nbsp;Add/Edit</button>
-                <div class="modal fade" id="delete_confirmation" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button class="btn btn-success btn-sm" data-toggle="modal" data-target="#add_address"><i class="fa fa-edit fa-lg"></i>&nbsp;Add/Edit</button>
+                <div class="modal fade" id="add_address" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                   <div class="modal-dialog">
                     <div class="modal-content">
                       <div class="modal-header">
@@ -148,9 +148,78 @@
             </div>
           </div>
         </div>
+        <div class="row">
+          <div class="col-md-3">
+            <h3>Emergency Contact</h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-3">
+          {{$patient->emergency_contact_name}}
+          </div>
+          <div class="col-md-3">
+          {{$patient->emergency_contact_phone}}
+          </div>
+          <div class="col-md-3">
+          {{$patient->emergency_contact_relationship}}
+          </div>
+          <div class="col-md-3">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button class="btn btn-success btn-sm" data-toggle="modal" data-target="#add_emergency"><i class="fa fa-edit fa-lg"></i>&nbsp;Add/Edit</button>
+          <div class="modal fade" id="add_emergency" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Add/Edit Emergency Contact</h4>
+                </div>
+                <div class="modal-body">
+                  <form class="form-horizontal" id="emergency_form" method="POST" action="{{ asset('patients/store') }}">
+                    <fieldset>
+                      <div class="form-group">
+                        <div class="col-sm-3">
+                          <label for="emergencyName" class="col-lg-2 control-label">{{ trans('forms.new.patient.emergency.name') }}</label>
+                        </div>
+                        <div class="col-md-6">
+                          <input type="text" class="form-control" name="emergencyName" id="emergencyName" placeholder="Contact Name">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <div class="col-sm-3">
+                          <label for="emergencyPhone" class="col-lg-2 control-label">{{ trans('forms.new.patient.mobile') }}</label>
+                        </div>
+                        <div class="col-md-6">
+                          <input type="tel" class="form-control" name="emergencyPhone" id="emergencyPhone" placeholder="Contact Mobile">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <div class="col-sm-3">
+                          <label for="emergencyRelationship" class="col-lg-2 control-label">{{ trans('forms.new.patient.emergency.rel') }}</label>
+                        </div>
+                        <div class="col-md-6">
+                        <select class="form-control" name="emergencyRelationship" id="emergencyRelationship">
+                          <option value="mother">{{ trans('forms.new.patient.mother') }}</option>
+                          <option value="father">{{ trans('forms.new.patient.father') }}</option>
+                          <option value="brother">{{ trans('forms.new.patient.brother') }}</option>
+                          <option value="sister">{{ trans('forms.new.patient.sister') }}</option>
+                          <option value="husband">{{ trans('forms.new.patient.husband') }}</option>
+                          <option value="wife">{{ trans('forms.new.patient.wife') }}</option>
+                          <option value="other">Other</option>
+                        </select>
+                      </div>
+                      </div>
+                    </fieldset>
+                    <input type="hidden" name="form_name" value="emergencyForm" >
+                    <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times fa-lg"></i> Close</button>
+                    <button type="submit" class="btn btn-primary">Add Emergency Contact</button>
+                  </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   </div>
-  
 </div>
-
 @stop
